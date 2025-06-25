1b5788ea7c5400b2fc1dd03ad7214b7797c720ac
flomadner <flomadner@gmail.com>
2025-06-25 22:00:18 +0200

    Update PetDetail.jsx

--- frontend/src/components/PetDetail.jsx
+++ frontend/src/components/PetDetail.jsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from './Navbar';
 
-const API_BASE_URL = 'https://cc241045-10757.node.fhstp.cc/api';
+const API_BASE_URL = 'http://localhost:3000';
 
 function PetDetail() {
     const { id } = useParams();
