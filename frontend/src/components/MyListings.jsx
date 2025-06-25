d7396a099288c72cbc064660e0d224109ce80499
flomadner <flomadner@gmail.com>
2025-06-25 21:59:56 +0200

    Update MyListings.jsx

--- frontend/src/components/MyListings.jsx
+++ frontend/src/components/MyListings.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import Navbar from './Navbar';
 
-const API_BASE_URL = 'https://cc241045-10757.node.fhstp.cc/api';
+const API_BASE_URL = 'http://localhost:3000';
 
 function MyListings() {
     const [listings, setListings] = useState([]);
