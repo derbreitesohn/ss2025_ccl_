1ec1dd32006c4a1bedde3ed0a4c7bafe6f7568a3
flomadner <flomadner@gmail.com>
2025-06-25 21:58:58 +0200

    Update Favorites.jsx

--- frontend/src/components/Favorites.jsx
+++ frontend/src/components/Favorites.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import Navbar from "./Navbar.jsx";
 import { FaHeart } from 'react-icons/fa';
 
-const API_BASE_URL = 'https://cc241045-10757.node.fhstp.cc/api';
+const API_BASE_URL = 'http://localhost:3000';
 
 function Favorites() {
     const [favorites, setFavorites] = useState([]);
